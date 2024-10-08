1b71d189ed74bdcc057fa468090dcc07ba29a82b
Arlynia <jdoherty1985@gmail.com>
2024-10-07 21:02:12 -0500

    home -> cal nav + cal styling v1.0

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Calendar from "./calendar";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function AppHomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable
+        onPress={() => {
+          console.log("Button Pressed");
+          navigation.navigate("Calendar");
+        }}
+      >
+        <Text style={styles.calendarText}>Go to Calendar</Text>
+      </Pressable>
     </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={AppHomeScreen} />
+        <Stack.Screen name="Calendar" component={Calendar} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  calendarText: {
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 25,
+    backgroundColor: "purple",
+    color: "white",
+    textAlign: "center",
   },
 });
