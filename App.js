4d6125adf0a57a07097c39b052a1fa860719b26c
DylanHart12 <123123805+DylanHart12@users.noreply.github.com>
2024-10-12 14:14:17 -0500

    Added Movement Functionality from Calender Homescreen to To-Do list

--- App.js
+++ App.js
@@ -1,104 +1,49 @@
-import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Keyboard, KeyboardAvoidingView, TextInput, Platform, StyleSheet, Text, View, Pressable } from 'react-native';
-import Task from './components/Task';
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ToDoList from "./ToDoList";
 
-export default function App() {
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
+const Stack = createNativeStackNavigator();
 
-  const handleAddTask = () => {
-    setTaskItems([...taskItems, task])
-    setTask(null);
-  }
-
-  const completeTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index,1);
-    setTaskItems(itemsCopy);
-    }
-
+function AppHomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      {/* Task to do list UI*/}
-      <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Today's tasks</Text>
-      
-        <View style={styles.items}>
-          {/*This is where the taskes will go*/}
-          {
-            taskItems.map((item, index) => {
-              return (
-              <Pressable key={index} onPress={() => completeTask(index)}>
-                <Task Text={item}/>
-              </Pressable>
-              )
-            })
-          }
-        </View>
-      </View>
-
-      {/*Write a Task*/ }
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
+      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable
+        onPress={() => {
+          console.log("Button Pressed");
+          navigation.navigate("ToDoList");
+        }}
       >
-        <TextInput style={styles.input} placeholder={'write a task'} value={task} onChangeText={text => setTask(text)}/>
-
-        <Pressable onPress ={() => handleAddTask()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
-          </View>
-        </Pressable>
-      </KeyboardAvoidingView>
-
+        <Text style={styles.toDoListText}>Go to To Do list</Text>
+      </Pressable>
     </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={AppHomeScreen} />
+        <Stack.Screen name="ToDoList" component={ToDoList} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED',
+    backgroundColor: "#fff",
   },
-  tasksWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
+  toDoListText: {
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 25,
+    backgroundColor: "purple",
+    color: "white",
+    textAlign: "center",
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-
-  },
-  items: {
-    marginTop: 30,
-  },
-  writeTaskWrapper: {
-    position: 'absolute',
-    bottom: 60,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: 250,
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-  },
-  addText: {},
 });
---
+++ ToDoList.js
@@ -0,0 +1,104 @@
+import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Keyboard, KeyboardAvoidingView, TextInput, Platform, StyleSheet, Text, View, Pressable } from 'react-native';
+import Task from './components/Task';
+
+export default function ToDoList() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index,1);
+    setTaskItems(itemsCopy);
+    }
+
+  return (
+    <View style={styles.container}>
+      {/* Task to do list UI*/}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+      
+        <View style={styles.items}>
+          {/*This is where the taskes will go*/}
+          {
+            taskItems.map((item, index) => {
+              return (
+              <Pressable key={index} onPress={() => completeTask(index)}>
+                <Task Text={item}/>
+              </Pressable>
+              )
+            })
+          }
+        </View>
+      </View>
+
+      {/*Write a Task*/ }
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'write a task'} value={task} onChangeText={text => setTask(text)}/>
+
+        <Pressable onPress ={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </Pressable>
+      </KeyboardAvoidingView>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
+});
