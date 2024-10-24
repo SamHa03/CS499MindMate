5cedeb0f76a260bb70ffc9f0573e23065b61ae17
DylanHart12 <123123805+DylanHart12@users.noreply.github.com>
2024-10-23 21:50:14 -0500

    Added Comments

--- BreathingAction.js
+++ BreathingAction.js
@@ -2,23 +2,26 @@ import React, {useEffect, useRef, useState } from "react";
 import { Animated, Text, View, StyleSheet, Pressable} from 'react-native';
 
 const BreathingAction = () => {
-    const scaleValue = useRef(new Animated.Value(1)).current;
+    const scaleValue = useRef(new Animated.Value(1)).current; //Opacity used for the animation
 
-    const [breathingHistory, setBreathingHistory] = useState([]);
+    const [breathingHistory, setBreathingHistory] = useState([]); //Breathing history array
 
-    const [breathingButtonToggle, setBreathingButtonToggle] = useState(false);
+    const [breathingButtonToggle, setBreathingButtonToggle] = useState(false); //Toggle that decides whether the breathing excersice button is pressable
 
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState(""); // Breath in, hold and out variable
 
-    const[time, setTime] = useState(0);
-    const[isRunning, setIsRunning] = useState(false);
+    const[time, setTime] = useState(0); //Time that shows for what section the breathing exercise is on
+    const[isRunning, setIsRunning] = useState(false); //Toggle if the timer is running or not
 
-
-    function addToHistory(){
+    //Adds to the history array
+    function addToHistory(){ 
         setBreathingHistory([...breathingHistory, 
             {date: new Date().toLocaleDateString(), time: new Date().toLocaleTimeString()}]);
     }
 
+    /* effect that waits for the change of isRunning for true.
+    If true then it starts the timer. If false it stops the timer and resets time to 1;
+    */
     useEffect(() => {
         setTime(1);
         let interval;
@@ -32,21 +35,28 @@ const BreathingAction = () => {
         return () => clearInterval(interval);
     }, [isRunning]);
 
+    //Animation steps for the circle expanding and shrinking
+    //Since the function executes everthing at once without waiting for the timeout 
+    //  of the previous stage to finish the different stages run after waiting for 
+    //  a combined time of all of the previous stages.
+    //For example the "Hold your breath stage" executes after 4 seconds of waiting rather than
+    //  until for timeing to finish.
     const BreathingCircleAnimation = ()=> {
-        setMessage("Breathe in");
-        Animated.timing(scaleValue, {
+        setMessage("Breathe in"); //Sets the message to "Breath in"
+        
+        Animated.timing(scaleValue, { //Expands the circle over 4 seconds
             toValue: 2,
             duration: 4000,
             useNativeDriver: true,
         }).start();
         
-        setTimeout(() => {
+        setTimeout(() => { //The circle doesn't move, however the timer does reset
             setMessage("Hold your Breath");
             setIsRunning(false);
             setIsRunning(true);
         }, 4000)
 
-        setTimeout(() => {
+        setTimeout(() => { //Circle shrinks, message changes
             setMessage("Breath Out");
             Animated.timing(scaleValue, {
                 toValue: 1,
@@ -58,7 +68,7 @@ const BreathingAction = () => {
             setIsRunning(true);
         }, 11000)
 
-        setTimeout(() => {
+        setTimeout(() => { //Completely resets the timer and re-enables the breathing exercise button
             setBreathingButtonToggle(false);
             setIsRunning(false);
         }, 19000)
@@ -67,17 +77,20 @@ const BreathingAction = () => {
     return (
         <View style={styles.container}>
 
-            <Text>{message}</Text>
+            {/*Message that shows In, Hold, and out messages*/}
+            <Text>{message}</Text> 
 
+            {/*Shows the timer*/}
             <Text style={styles.timerText}>{time}</Text>
             
+            {/*Cirlce that expands is also a pressable*/}
             <Pressable disabled={breathingButtonToggle} onPress={() => {
-                setBreathingButtonToggle(true); 
-                setIsRunning(true); 
-                BreathingCircleAnimation();
-                addToHistory();
+                setBreathingButtonToggle(true); //Disables pressable
+                setIsRunning(true); //Starts the timer
+                BreathingCircleAnimation(); //Begins the animation
+                addToHistory(); //Adds the session to the array
                 }}>
-                <Animated.View
+                <Animated.View //Animated view that allows for the transformation of the circle
                 style={[
                     styles.circle,
                     {transform: [{scale: scaleValue}]}
