d224632b02a310692588320c3026bd17b65eb19a
Arlynia <jdoherty1985@gmail.com>
2024-10-25 14:07:52 -0500

    Btm Tab Nav Implementation

---
+++ Screens/tasks.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function TimerScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>This is edit / add tasks page.</Text>
+    </View>
+  );
+}
+
+// styling begins here
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "flex-start",
+    backgroundColor: "#F2EEE9",
+    flex: 1,
+    alignItems: "center",
+  }
+});
